5161af2492a0aaeacc7fbe3c555a846b113a5c10
Kaustubh <kaustubhsharma999@gmail.com>
2024-05-21 13:39:03 +0530

    init

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const avatarAppConfig = {
+    cogSvcRegion: "westus2",
+    cogSvcSubKey: "595b92ac299c414d817b5688f932b4dc",
+    voiceName: "en-US-JennyNeural",
+    avatarCharacter: "lisa",
+    avatarStyle: "casual-sitting",
+    avatarBackgroundColor: "#FFFFFFFF",
+    iceUrl: "turn:relay.communication.microsoft.com:3478",
+    iceUsername: "BQAANhay0gAB2q39BMtIGQ3eEa7NO1t6WfD/PWr2p0YAAAAQAxC6Y2laD2dOgrYx1Zk1SkZ9nauvQ0LgszzzwEWPBED0jrNPpLs=",
+    iceCredential: "NVD9aBRHKj3CVJOAOjUE2R+JrqI="
+}
